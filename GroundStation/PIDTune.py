844aeb3aa860c6e510488d48de24b0cc9a74b74e
Ian <spyguyian@gmail.com>
2018-03-13 09:39:46 -0400

    Started Python Ground Station

---
+++ GroundStation/PIDTune.py
@@ -0,0 +1,3 @@
+#TODO Comms with Arduino (class)
+#TODO Visualise data, maybe plot graphs and/or 3d view
+#TODO make GUI input nice
---
+++ GroundStation/helpers/DataMap.py
@@ -0,0 +1,4 @@
+import serial
+
+ser = serial.Serial('COM7', 9600)
+time.sleep(2)
